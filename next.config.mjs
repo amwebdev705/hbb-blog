ee287e23a2db78e43ba4e303160c43191f340de8
amwebdev705 <amwebdev705@gmail.com>
2025-01-17 17:07:44 -0800

    fix import from clerk

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-export default nextConfig;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    externalDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    }
+    return config
+  },
+}
+
+export default nextConfig
