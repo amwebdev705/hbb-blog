4c47db6a805acaa5f2d31dc09fae6b54367ec74d
amwebdev705 <amwebdev705@gmail.com>
2025-01-17 16:20:31 -0800

    Add clerk auth

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,13 +1,15 @@
-'use client';
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import Link from 'next/link';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { usePathname } from 'next/navigation';
-import { useTheme } from 'next-themes';
+'use client'
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import Link from 'next/link'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
+import { useTheme } from 'next-themes'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+import { dark, light } from '@clerk/themes'
 export default function Header() {
-  const path = usePathname();
-  const { theme, setTheme } = useTheme();
+  const path = usePathname()
+  const { theme, setTheme } = useTheme()
   return (
     <Navbar className='border-b-2'>
       <Link
@@ -15,7 +17,7 @@ export default function Header() {
         className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
       >
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-          Harla's
+          Harla&apos;s
         </span>
         Blog
       </Link>
@@ -39,11 +41,20 @@ export default function Header() {
         >
           {theme === 'light' ? <FaSun /> : <FaMoon />}
         </Button>
-        <Link href='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
-          </Button>
-        </Link>
+        <SignedIn>
+          <UserButton
+            appearance={{
+              baseTheme: theme === 'light' ? light : dark,
+            }}
+          />
+        </SignedIn>
+        <SignedOut>
+          <Link href='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        </SignedOut>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
@@ -64,5 +75,5 @@ export default function Header() {
         </Link>
       </Navbar.Collapse>
     </Navbar>
-  );
+  )
 }
